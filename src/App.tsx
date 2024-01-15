88611b07ed77b57165f378bc398449eae85f7429
pratiksatpathy <pratik.satpathy@audviklabs.com>
2024-01-15 19:22:54 +0530

    Buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {  Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from './pages/Home/Home';
@@ -6,17 +6,34 @@ import VenueDetails from './pages/VenueDetails/VenueDetails';
 import EventDetails from './pages/EventDetails/EventDetails';
 
 function App() {
-
+  const navigate = useNavigate()
   return (
     <div>
       Welcome to Kruti's Wedding
-      <BrowserRouter basename="/kruti_wedding.github.io">
+      
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => {
+            navigate("/venue-details");
+          }}
+        >
+          Venue
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => {
+            navigate("/event-details");
+          }}
+        >
+          Event
+        </button>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/venue-details" element={<VenueDetails />} />
           <Route path="/event-details" element={<EventDetails />} />
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }
