abc95a6066900023c68929d4953a6db8c79fddf7
pratiksatpathy <pratik.satpathy@audviklabs.com>
2024-01-15 15:42:31 +0530

    Initial push

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+  return (
+    <>
+      <h1>
+        Hi
+      </h1>
+    </>
+  )
+}
+
+export default App
