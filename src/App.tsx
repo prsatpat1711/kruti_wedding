266c466dfa3061da732629568b48d1ea0d28dcf5
pratiksatpathy <pratik.satpathy@audviklabs.com>
2024-01-15 16:13:04 +0530

    Initial push

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from './pages/Home/Home';
+import VenueDetails from './pages/VenueDetails/VenueDetails';
+import EventDetails from './pages/EventDetails/EventDetails';
 
 function App() {
 
   return (
     <>
-      <h1>
-        Hi
-      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='' element={<Home/>}/>
+          <Route path='/venue-details' element={<VenueDetails/>}/>
+          <Route path='/event-details' element={<EventDetails/>}/>          
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/VenueDetails/VenueDetails.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function VenueDetails() {
+  return <div>Venue Details</div>;
+}
